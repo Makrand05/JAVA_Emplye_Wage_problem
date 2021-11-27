5d07279f3e8f5483eef34a2708fda6ea8aa1f2f4
Makrand <makrandshingare@gmail.com>
2021-11-27 08:26:57 +0530

    Check the employee status

--- src/Employee_Wage_Problem_using_OOP.java
+++ src/Employee_Wage_Problem_using_OOP.java
@@ -1,6 +1,12 @@
 public class Employee_Wage_Problem_using_OOP {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int status=((int)(Math.random()*10)%2);
+        System.out.println(status);
+        if(status==1) {
+            System.out.println("Employee is present");
+        }
+        else
+            System.out.println("Employee is absent");
     }
 
 }
