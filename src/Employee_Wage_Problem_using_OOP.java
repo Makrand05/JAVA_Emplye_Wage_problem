f67861503e8f6a0408ffda415e8dab9ba2832064
Makrand <makrandshingare@gmail.com>
2021-11-27 21:26:18 +0530

    [Makrand]  Added part time employee and wage

--- src/Employee_Wage_Problem_using_OOP.java
+++ src/Employee_Wage_Problem_using_OOP.java
@@ -1,18 +1,22 @@
 public class Employee_Wage_Problem_using_OOP {
     public static void main(String[] args) {
-        int IS_FULL_TIME=1;
-        int PER_HOUR=20;
-        int empHrs=0;
-        int empWage=0;
-        int status=((int)(Math.random()*10)%2);
-        System.out.println(status);
-        if(status==IS_FULL_TIME) {
-            empHrs=8;
-        }
-        else
-            empHrs=0;
-        empWage=empHrs * PER_HOUR;
-        System.out.println("Employee wage : "+ empWage);
+        int IS_FULL_TIME = 1;
+        int IS_PART_TIME = 2;
+        int PER_HOUR = 20;
+        int FULL_TIME = 8;
+        int PART_TIME = 4;
+        int empHrs = 0;
+        int empWage = 0;
+        int status = ((int) (Math.random() * 10) % 3);
+//        System.out.println(status);
+        if (status == IS_FULL_TIME) {
+            empHrs = 8;
+        } else if (status == IS_PART_TIME) {
+            empHrs = 4;
+        } else
+            empHrs = 0;
+        empWage = empHrs * PER_HOUR;
+        System.out.println("Employee wage : " + empWage);
     }
 
 }
