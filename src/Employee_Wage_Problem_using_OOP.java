ee8e99fbe83cd370bb7def618001783b97703c33
Makrand <makrandshingare@gmail.com>
2021-11-27 09:07:54 +0530

    Calculated Employee Wage

--- src/Employee_Wage_Problem_using_OOP.java
+++ src/Employee_Wage_Problem_using_OOP.java
@@ -1,12 +1,18 @@
 public class Employee_Wage_Problem_using_OOP {
     public static void main(String[] args) {
+        int IS_FULL_TIME=1;
+        int PER_HOUR=20;
+        int empHrs=0;
+        int empWage=0;
         int status=((int)(Math.random()*10)%2);
         System.out.println(status);
-        if(status==1) {
-            System.out.println("Employee is present");
+        if(status==IS_FULL_TIME) {
+            empHrs=8;
         }
         else
-            System.out.println("Employee is absent");
+            empHrs=0;
+        empWage=empHrs * PER_HOUR;
+        System.out.println("Employee wage : "+ empWage);
     }
 
 }
